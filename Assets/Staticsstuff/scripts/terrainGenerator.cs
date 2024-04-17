7df9cfebdca1f3ccec93759a8a23f71b32323bdf
Mauro Hendriks <89855665+Mauro-Hendriks@users.noreply.github.com>
2024-04-17 16:41:35 +0200

    test noise voor landscape
    http://kitfox.com/projects/perlinNoiseMaker/
    https://www.youtube.com/watch?v=vFvwyu_ZKfU

--- Assets/Staticsstuff/scripts/terrainGenerator.cs
+++ Assets/Staticsstuff/scripts/terrainGenerator.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class terrainGenerator : MonoBehaviour
+namespace Staticsstuff.scripts
 {
-    // Start is called before the first frame update
-    void Start()
+    public class terrainGenerator : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private Texture2D _noiseTexture;
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+            Terrain terrain = GetComponent<Terrain>();
+            GenerateHeights(terrain.terrainData);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
+        }
+        void GenerateHeights(TerrainData data) {
+            float[,] heights = new float[data.heightmapResolution, data.heightmapResolution];
+            for (int x = 0; x < data.heightmapResolution; x++) {
+                for (int y = 0; y < data.heightmapResolution; y++)
+                {
+                    heights[x, y] = _noiseTexture.GetPixel(x, y).grayscale;
+                }
+            }
+            data.SetHeights(0, 0, heights);
+        }
     }
 }
