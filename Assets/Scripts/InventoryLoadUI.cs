ae2604d041ae03b843f698a83e7741a0e195641d
Rutger1111 <90682560+Rutger1111@users.noreply.github.com>
2024-05-10 20:05:43 +0200

    UI of inventory done

--- Assets/Scripts/InventoryLoadUI.cs
+++ Assets/Scripts/InventoryLoadUI.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryLoadUI : MonoBehaviour
 {
+    public float distance;
     public GameObject Ingredient;
     public GameObject content;
     public inventoryHolder Inventory;
@@ -19,7 +22,7 @@ public class InventoryLoadUI : MonoBehaviour
     {
         while(counter < Inventory.Inventory.Count)
         {
-            int quantity = Inventory.RequestAmount(names[counter - 1]);
+            int quantity = Inventory.RequestAmount(names[counter]);
             Debug.Log(quantity);
             SpawnIn(quantity);
             counter ++;
@@ -30,5 +33,8 @@ public class InventoryLoadUI : MonoBehaviour
     {
         Debug.Log("came here");
         GameObject Ingrediente = Instantiate(Ingredient, content.transform);
+        Ingrediente.transform.position = new Vector3(Ingrediente.transform.position.x,Ingrediente.transform.position.y - distance * counter + 1,Ingrediente.transform.position.z);
+        Ingrediente.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = names[counter];
+        Ingrediente.transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = ""+Amount;
     }
 }
