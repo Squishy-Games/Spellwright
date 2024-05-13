7bc536230ae17d38bb2c9c01736276af8f9bc526
Gino Schaap <35615@ma-web.nl>
2024-05-13 20:29:28 +0200

    made the variables serializefield and removed the hardcoded values :)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-    public float walkingSpeed = 7.5f;
-    public float runningSpeed = 11.5f;
-    public float jumpSpeed = 8.0f;
-    public float gravity = 20.0f;
-    public Camera playerCamera;
-    public float lookSpeed = 2.0f;
-    public float lookXLimit = 45.0f;
+    [SerializeField] private float walkingSpeed;
+    [SerializeField] private float runningSpeed;
+    [SerializeField] private float jumpSpeed;
+    [SerializeField] private float gravity;
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private float lookSpeed;
+    [SerializeField] private float lookXLimit;
 
     CharacterController characterController;
     Vector3 moveDirection = Vector3.zero;
