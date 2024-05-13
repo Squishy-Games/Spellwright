cfdd720b0d67f53d8fb13b7a4053e414e7f21f18
Rutger1111 <90682560+Rutger1111@users.noreply.github.com>
2024-05-13 22:56:09 +0200

    throw
    je kan nu gooien

--- Assets/Scripts/crafting/drag.cs
+++ Assets/Scripts/crafting/drag.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
@@ -8,21 +9,42 @@ public class drag : MonoBehaviour
     public Vector3 lastMousePosition;
     public bool IsDragging = true;
     public bool isOver;
+    public float launchspeed = 2;
+    public int framesInBetween;
+    public Dictionary<int,Vector3> LastPositions;
+    public int PositionIndex; 
     // Update is called once per frame
-    void Update()
+     void FixedUpdate() 
     {
+        StartCoroutine(wait());
+
         bool check = IsDragging;
-        IsDragging = (Input.GetKey(KeyCode.Mouse0) == true && isOver == true) ? true : false;
-        if(IsDragging == true)
+
+        if(Input.GetKeyDown(KeyCode.Mouse0) && isOver == true)
+        {
+            IsDragging = true;
+        }
+
+        if(Input.GetKey(KeyCode.Mouse0) == true && IsDragging == true)
         {
             transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 8);
             transform.GetComponent<Rigidbody2D>().gravityScale = 0;
             transform.GetComponent<Rigidbody2D>().velocity = new Vector3(0,0,0);
         }
-        else {
+        else if (IsDragging == true){
+            IsDragging = false;
+            Debug.Log(IsDragging);
             transform.GetComponent<Rigidbody2D>().gravityScale = 1;
+            float frame = 0.01666666666f;
+            transform.GetComponent<Rigidbody2D>().velocity = new Vector3((Input.mousePosition.x - lastMousePosition.x) * frame * launchspeed,(Input.mousePosition.y - lastMousePosition.y) * frame * launchspeed, (Input.mousePosition.z + lastMousePosition.z) * frame * launchspeed);
+            Debug.Log("speed" + new Vector3((Input.mousePosition.x + lastMousePosition.x) * frame,(Input.mousePosition.y + lastMousePosition.y) * frame, (Input.mousePosition.z + lastMousePosition.z) * frame));
         }
     }
+    public IEnumerator<WaitForEndOfFrame> wait(){
+        yield return new WaitForEndOfFrame();
+        lastMousePosition = Input.mousePosition;
+    }
+    
     void OnMouseOver()
     {
         isOver = true;
